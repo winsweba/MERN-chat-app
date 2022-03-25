12c8a341268b7d428efbfa1a1f17702206b2711a
winsweba <winsweb@outlook.com>
2022-03-25 10:22:02 -0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+dotenv.config();
+connectDB();
+const app = express();
+app.use(express.json()); // to accept JSON Data
+
+app.get("/", (req, res) => {
+  res.send("API is Running");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+//npm i dotenv  && nodemon
+const PORT = process.env.PORT;
+
+const server = app.listen(
+  PORT,
+  console.log(`server has start NO: ${PORT}`.yellow.bold)
+);
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to socket");
+
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    // console.log(userData._id);
+    socket.emit("connected ");
+  });
+
+
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+});
+
